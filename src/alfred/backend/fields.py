3e0620fd96de29243729722aa81bb00ee3d2be7a
rhdzmota <contact@rhdzmota.com>
2023-03-19 22:40:23 -0600

    ALF-5 Create custom text enum fields: lang, gender

---
+++ src/alfred/backend/fields.py
@@ -0,0 +1,18 @@
+import enum
+
+from alfred.backend.interface import TextEnum, TextEnumField
+
+class LangField(TextEnumField):
+
+    class Enum(TextEnum):
+        ALL = enum.auto()
+        EN = "english"
+        ES = "spanish"
+
+class GenderField(TextEnumField):
+
+    class Enum(TextEnum):
+        UNDEFINED = enum.auto()
+        OTHER = enum.auto()
+        FEMALE = enum.auto()
+        MALE = enum.auto()
