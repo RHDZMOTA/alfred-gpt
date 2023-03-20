0c953ba9e94c369dea1bf5de050da1885e89af12
rhdzmota <contact@rhdzmota.com>
2023-03-20 16:37:09 -0600

    ALF-7 Create database connection module

--- src/alfred/dao/database.py
+++ src/alfred/dao/database.py
@@ -1,11 +1,4 @@
-import datetime as dt
-
 import peewee
-from peewee import (
-    SqliteDatabase,
-    DateTimeField,
-    Model,
-)
 
 from alfred.settings import (
     ALFRED_DEFAULT_SQLITE,
@@ -20,21 +13,8 @@ try:
     raise NotImplementedError
 except NotImplementedError:
     logger.error("Database not implemented; using sqlite instead.")
-    db = SqliteDatabase(ALFRED_DEFAULT_SQLITE)
+    db = peewee.SqliteDatabase(ALFRED_DEFAULT_SQLITE)
 except Exception as e:
     logger.error("Error encountered when trying to connect to database; falling back to sqlite database")
     logger.exception(e)
-    db = SqliteDatabase(ALFRED_DEFAULT_SQLITE)
-
-
-class BaseModel(Model):
-    created_at = DateTimeField(default=dt.datetime.utcnow, null=False)
-    updated_at = DateTimeField(default=dt.datetime.utcnow, null=True)
-
-    @classmethod
-    def get_database(cls) -> peewee.Database:
-        # Doing this for autocomplete reasons
-        return getattr(getattr(cls, "_meta"), "database")
-
-    class Meta:
-        database = db
+    db = peewee.SqliteDatabase(ALFRED_DEFAULT_SQLITE)
