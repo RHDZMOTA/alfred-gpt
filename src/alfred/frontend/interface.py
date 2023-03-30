1e9001126ea27edc996cb0592a3e51b08dfc67cc
rhdzmota <contact@rhdzmota.com>
2023-03-30 06:58:29 -0600

    ALF-15 Define view attributes and context manager

--- src/alfred/frontend/interface.py
+++ src/alfred/frontend/interface.py
@@ -1,13 +1,22 @@
 import datetime as dt
 from typing import Optional
+from types import TracebackType
+
 
 from alfred.models.user import User
-from alfred.frontend.functions import get_user_with_warning
+from alfred.settings import get_logger
+from alfred.frontend.functions import get_user
 from alfred.utils.string_ops import camel_to_snake
+
+
+logger = get_logger(name=__name__)
 
 
 class ViewInterface:
     order_reference: int
+    login_required: bool = False
+    disable_user_info: bool = False
+    hidden_if_session_active: bool = False
     _instance = None
 
     def __init__(self):
@@ -23,6 +32,37 @@ class ViewInterface:
             cls._instance = cls.__new__(cls)
         return cls._instance
 
+    def __enter__(self):
+        user_instance = self.user
+        if self.login_required and not user_instance:
+            return None
+        # Should we disable the user info?
+        if self.disable_user_info:
+            if self.hidden_if_session_active and user_instance:
+                return None
+            return self
+        # Should we display the user info?
+        if user_instance:
+            import streamlit as st
+            st.info(f"Logged as {user_instance.name}")
+        if self.hidden_if_session_active and user_instance:
+            return None
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Optional[type[BaseException]],
+            exc_val: Optional[BaseException],
+            exc_tb: Optional[TracebackType],
+    ):
+        ok = exc_val is None
+        logger.info(
+            "View (%s) execution: %s",
+            self.alias,
+            "successful" if ok else "failure"
+        )
+
+
     @property
     def alias(self) -> str:
         name = getattr(self, "__name__")  # Why not self.__name__? Because mypy...
@@ -35,7 +75,7 @@ class ViewInterface:
 
     @property
     def user(self) -> Optional[User]:
-        return get_user_with_warning()
+        return get_user()
 
     def run(self):
         raise NotImplementedError
